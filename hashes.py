91f75b2d31652d30bceb45b2f0d38a0a0fa88b68
Rory Goodman <goodmanr@tcd.ie>
2018-10-19 19:53:02 +0100

    python script

---
+++ hashes.py
@@ -0,0 +1,13 @@
+__author__ = 'rorygoodman'
+hashfile = open("des.txt", "r")
+newfile=open("goodmanr.broken", "w")
+lines = hashfile.readlines()
+
+for line in lines:
+    if len(line.rstrip().replace(':',' '))==22:
+        newfile.write(line.rstrip().replace(':',' ')[0:21]+"\n")
+    else:
+        newfile.write(line.rstrip().replace(':',' ')+"\n")
+
+
+
